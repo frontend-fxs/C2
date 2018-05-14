358b701bb234bd97539a93fb105ba1fcfaa63550
frontend-fxs <oriol@fxstreet.com>
2018-05-14 17:29:17 +0200

    up

--- js/fxstreet-class-ecocal.js
+++ js/fxstreet-class-ecocal.js
@@ -1,5 +1,18 @@
+var event = {
+  tradeable: true,
+  time: new Date(),
+  volatility: 0,
+  title: 'event title',
+  flag: 'flagname',
+  actual: 27,
+  deviation: 107,
+  consensus: 107,
+  previous: -1.7,
+  dashboardLink: 'google.es'
+}
+
 $.get('https://frontend-fxs.github.io/C2/js/templates/row.mst', function (template) {
   Mustache.parse(template)
-  var rendered = Mustache.render(template, { name: 'Oriol' })
+  var rendered = Mustache.render(template, event)
   $('#targetRender').html(rendered)
 })

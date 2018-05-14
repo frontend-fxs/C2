b0d722a2a7324d600bcfdc24f470ab67455c45ad
frontend-fxs <oriol@fxstreet.com>
2018-05-14 17:10:41 +0200

    up

--- js/fxstreet-class-ecocal.js
+++ js/fxstreet-class-ecocal.js
@@ -1,15 +1,5 @@
-var init = function(){
-  setVars();
-  render();
-  addEvents();
-};
-var setVars = function () {}
-var render = function () {
-  $.get('./js/templates/row.mst', function(template) {
-    Mustache.parse(template);
-    var rendered = Mustache.render(template, { name:'Oriol' } );
-    $('#targetRender').html(rendered);
-  });
-} 
-var addEvents = function () { }; 
-init();
+$.get('https://frontend-fxs.github.io/C2/js/templates/row.mst', function (template) {
+  Mustache.parse(template)
+  var rendered = Mustache.render(template, { name: 'Oriol' })
+  $('#targetRender').html(rendered)
+})

de6903c570d102052434e0695a4697b4fa31330c
frontend-fxs <oriol@fxstreet.com>
2018-05-15 14:44:06 +0200

    up

--- js/fxstreet-class-ecocal.js
+++ js/fxstreet-class-ecocal.js
@@ -41,7 +41,7 @@ var event = {
   DashboardLink: 'google.es'
 }
 
-$.get('https://frontthis.End-fxs.github.io/C2/js/templates/row.mst', function (template) {
+$.get('https://frontend-fxs.github.io/C2/js/templates/row.mst', function (template) {
   Mustache.parse(template)
   var rendered = Mustache.render(template, event)
   $('#eventRow').html(rendered)

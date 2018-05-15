78857eba28454bf00ceb0119fcfd030d555368ef
frontend-fxs <oriol@fxstreet.com>
2018-05-15 17:51:32 +0200

    up

--- js/fxstreet-class-ecocal.js
+++ js/fxstreet-class-ecocal.js
@@ -1,20 +1,49 @@
-var Translations = {
+var translations = {
   In: 'in ',
   Now: 'NOW',
   SecondsLabel: '"',
   MinutesLabel: "'",
   TimeStampSeparator: ':'
 }
-var event = {
+var time = {
+  Year: 2018,
+  Month: 4,
+  Day: 15,
+  Hour: 17,
+  Minute: 45
+}
+var calcCountDownString = function (end, now) {
+  var countdownMilliseconds = end.getTime() - now.getTime()
+  var countdownSeconds = parseInt(countdownMilliseconds / 1000)
+  var countdownMinutes = parseInt(countdownMilliseconds / 1000 * 60)
+  var timeStampString = end.toLocaleTimeString('en-us', { hour: '2-digit', minute: '2-digit', hour12: false })
+  var countdownString = ''
+  switch (true) {
+    case countdownMilliseconds <= 0:
+      countdownString = translations.Now
+      break
+    case countdownMilliseconds < 60000:
+      countdownString = translations.In + countdownSeconds + translations.SecondsLabel
+      break
+    case countdownMilliseconds < 3600000:
+      countdownString = translations.In + countdownMinutes + translations.MinutesLabel
+      break
+    default:
+      countdownString = timeStampString
+      break
+  }
+  return countdownString
+}
+var getCountDownString = function (time) {
+  var end = new Date(time.year, time.month, time.day, time.hour, time.minute)
+  var now = new Date()
+  var countdownString = calcCountDownString(end, now)
+  return countdownString
+}
+var countDown = getCountDownString(time)
+var ecoCalEvent = {
   Tradeable: true,
-  Time: {
-    Year: 2018,
-    Month: 4,
-    Day: 15,
-    Hour: 17,
-    Minute: 45
-  },
-  CountDown: getTime(this.Time),
+  CountDown: countDown,
   Volatility: 0,
   Title: 'event title',
   Flag: 'flagname',
@@ -24,38 +53,8 @@ var event = {
   Previous: -1.7,
   DashboardLink: 'google.es'
 }
-
-var getTime = function (time) {
-  var end = new Date(time.year, time.month, time.day, time.hour, time.minute)
-  var now = new Date()
-  var countdownString = calcCountDownString(end,now)
-  return countdownString
-}
-var calcCountDownString = function (end,now) {
-  var countdownMilliseconds = end.getTime() - now.getTime()
-  var countdownSeconds = parseInt(countdownMilliseconds / 1000)
-  var countdownMinutes = parseInt(countdownMilliseconds / 1000 * 60)
-  var timeStampString = end.toLocaleTimeString('en-us', { hour: '2-digit', minute: '2-digit', hour12: false })
-  var countdownString = "";
-  switch (true) {
-    case countdownMilliseconds <= 0:
-      countdownString = Translations.Now
-      break
-    case countdownMilliseconds < 60000:
-      countdownString = Translations.In + countdownSeconds + Translations.SecondsLabel
-      break
-    case countdownMilliseconds < 3600000:
-      countdownString = Translations.In + countdownMinutes + Translations.MinutesLabel
-      break
-    default:
-      countdownString = timeStampString
-      break
-  }
-  return countdownString
-}
-
 $.get('https://frontend-fxs.github.io/C2/js/templates/row.mst', function (template) {
   Mustache.parse(template)
-  var rendered = Mustache.render(template, event)
+  var rendered = Mustache.render(template, ecoCalEvent)
   $('#eventRow').html(rendered)
 })

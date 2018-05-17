0baf51f025f9b13e9d1e5242242c8b49b5725aaa
frontend-fxs <oriol@fxstreet.com>
2018-05-17 10:00:31 +0200

    up

---
+++ js/fxstreet-class-ecocal.js
@@ -0,0 +1,38 @@
+var renderData = function(){
+    $.get('https://frontend-fxs.github.io/C2/js/templates/data.mst', function (template) {
+        Mustache.parse(template)
+        var rendered = Mustache.render(template, ecoCalEventJson)
+        $('#fxs_ecocal_data').append(rendered)
+      })
+}
+
+var renderMenu = function(){
+    $.get('https://frontend-fxs.github.io/C2/js/templates/menu.mst', function (template) {
+        Mustache.parse(template)
+        var rendered = Mustache.render(template, ecoCalEventJson)
+        $('#fxs_ecocal_menu').append(rendered)
+      })
+}
+
+var renderFilter = function(){
+    $.get('https://frontend-fxs.github.io/C2/js/templates/filter.mst', function (template) {
+        Mustache.parse(template)
+        var rendered = Mustache.render(template, ecoCalEventJson)
+        $('#fxs_ecocal_filter').append(rendered)
+      })
+}
+
+
+var renderEcocal = function () {
+  $.get('https://frontend-fxs.github.io/C2/js/templates/ecocal.mst', function (template) {
+    Mustache.parse(template)
+    var rendered = Mustache.render(template, ecoCalEventJson)
+    $('#fxs_ecocal').append(rendered)
+    renderFilter();
+    renderMenu();
+    renderData();
+  })
+}
+
+
+renderEcocal();

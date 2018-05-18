76cfc202f939f1f1c894df70064011509673ff8e
frontend-fxs <oriol@fxstreet.com>
2018-05-18 11:16:07 +0200

    up

--- js/fxstreet-class-ecocal.js
+++ js/fxstreet-class-ecocal.js
@@ -10,109 +10,113 @@ var ecoCalEvents = {
   Periods: []
 }
 
-var now = null;
+var randomEcocalService = function () {
 
-var randomPeriodService = function (end) {
+  var today = new Date()
+  var tomorrow = new Date()
+  tomorrow.setDate(tomorrow.getDate() + 1)
+  var afterTomorrow = new Date()
+  afterTomorrow.setDate(afterTomorrow.getDate() + 2)
 
-  var getCountDownString = function () {
-    var countdownMilliseconds = end.getTime() - now.getTime()
-    var countdownSeconds = parseInt(countdownMilliseconds / 1000)
-    var countdownMinutes = parseInt(countdownMilliseconds / (1000 * 60))
-    var timeStampString = end.toLocaleTimeString('en-us', { hour: '2-digit', minute: '2-digit', hour12: false })
-    var countdownString = ''
-    switch (true) {
-      case countdownMilliseconds <= 0:
-        countdownString = translations.Now
-        break
-      case countdownMilliseconds < 60000:
-        countdownString = translations.In + countdownSeconds + translations.SecondsLabel
-        break
-      case countdownMilliseconds < 3600000:
-        countdownString = translations.In + countdownMinutes + translations.MinutesLabel
-        break
-      default:
-        countdownString = timeStampString
-        break
+  ecoCalEvents.Periods.push(randomPeriodService(today))
+  ecoCalEvents.Periods.push(randomPeriodService(tomorrow))
+  ecoCalEvents.Periods.push(randomPeriodService(afterTomorrow))
+
+  var now = null
+
+  var randomPeriodService = function (end) {
+    var getCountDownString = function () {
+      var countdownMilliseconds = end.getTime() - now.getTime()
+      var countdownSeconds = parseInt(countdownMilliseconds / 1000)
+      var countdownMinutes = parseInt(countdownMilliseconds / (1000 * 60))
+      var timeStampString = end.toLocaleTimeString('en-us', { hour: '2-digit', minute: '2-digit', hour12: false })
+      var countdownString = ''
+      switch (true) {
+        case countdownMilliseconds <= 0:
+          countdownString = translations.Now
+          break
+        case countdownMilliseconds < 60000:
+          countdownString = translations.In + countdownSeconds + translations.SecondsLabel
+          break
+        case countdownMilliseconds < 3600000:
+          countdownString = translations.In + countdownMinutes + translations.MinutesLabel
+          break
+        default:
+          countdownString = timeStampString
+          break
+      }
+      return countdownString
     }
-    return countdownString
-  }
 
-  var isFuture = function(){
-    return now.getTime() <= end.getTime()
-  }
-
-  var isTooLongPast = function(){
-    return !isFuture() && now.getTime() - end.getTime() > 120000;
-  }
-
-  var isNow = function(){
-    return isFuture() || !isTooLongPast()
-  }
-
-  var period = {
-    Header: date.toLocaleDateString('en-EN',{weekday:"narrow", month:"short", day:"2-digit"}),
-    Rows: []
-  }
-
-  for (var i = 1; i < 10; i++) {
-    var randomNumber = Math.random()
-    var time = {
-      Hour: Math.floor(Math.random() * Math.floor(24)) + 1,
-      Minute: Math.floor(Math.random() * Math.floor(59)) + 1
+    var isFuture = function () {
+      return now.getTime() <= end.getTime()
     }
-    var end = date.setHours(time.Hour)
-    var end = date.setMinutes(time.Minute)
-    var now = new Date()
 
-    var event = {
-      IsActive: !isTooLongPast(),
-      IsNow: isNow(),
-      Tradeable: randomNumber > 0.5,
-      CountDown: getCountDownString(),
-      Date: end,
-      VolatilityLevel: randomNumber > 0.1 && Math.floor(randomNumber * 3 + 1),
-      Flag: 'us',
-      Currency: 'EUR',
-      Title: 'CBI Distributive Trades Survey - Realized (MoM) (May)',
-      Actual: {
-        Value: '¥-9999.99B',
-        High: randomNumber > 0.66,
-        Low: randomNumber < 0.33
-      },
-      Deviation: {
-        Value: '-5.66%',
-        High: randomNumber > 0.66,
-        Low: randomNumber < 0.33
-      },
-      Consensus: '¥-9999.99B',
-      Previous: {
-        Value: '¥-9999.99B',
-        Revised: {
-          Tooltip: 'Revised From ',
+    var isTooLongPast = function () {
+      return !isFuture() && now.getTime() - end.getTime() > 120000
+    }
+
+    var isNow = function () {
+      return isFuture() || !isTooLongPast()
+    }
+
+    var period = {
+      Header: end.toLocaleDateString('en-EN', { weekday: 'narrow', month: 'short', day: '2-digit' }),
+      Rows: []
+    }
+
+    for (var i = 1; i < 10; i++) {
+      var randomNumber = Math.random()
+      var time = {
+        Hour: Math.floor(Math.random() * Math.floor(24)) + 1,
+        Minute: Math.floor(Math.random() * Math.floor(59)) + 1
+      }
+      var end = date.setHours(time.Hour)
+      var end = date.setMinutes(time.Minute)
+      var now = new Date()
+
+      var event = {
+        IsActive: !isTooLongPast(),
+        IsNow: isNow(),
+        Tradeable: randomNumber > 0.5,
+        CountDown: getCountDownString(),
+        Date: end,
+        VolatilityLevel: randomNumber > 0.1 && Math.floor(randomNumber * 3 + 1),
+        Flag: 'us',
+        Currency: 'EUR',
+        Title: 'CBI Distributive Trades Survey - Realized (MoM) (May)',
+        Actual: {
           Value: '¥-9999.99B',
           High: randomNumber > 0.66,
           Low: randomNumber < 0.33
-        }
-      },
-      DashboardLink: 'google.es'
+        },
+        Deviation: {
+          Value: '-5.66%',
+          High: randomNumber > 0.66,
+          Low: randomNumber < 0.33
+        },
+        Consensus: '¥-9999.99B',
+        Previous: {
+          Value: '¥-9999.99B',
+          Revised: {
+            Tooltip: 'Revised From ',
+            Value: '¥-9999.99B',
+            High: randomNumber > 0.66,
+            Low: randomNumber < 0.33
+          }
+        },
+        DashboardLink: 'google.es'
+      }
+      period.Rows.push(event)
     }
-    period.Rows.push(event)
+    period.Rows.sort(function (a, b) {
+      return a.Date - b.Date
+    })
+    return period
   }
-  period.Rows.sort(function (a, b) {
-    return a.Date - b.Date
-  })
-  return period
 }
 
-var today = new Date();
-var tomorrow = new Date();
-tomorrow.setDate(tomorrow.getDate() + 1);
-var afterTomorrow = new Date();
-afterTomorrow.setDate(afterTomorrow.getDate() + 2);
-
-ecoCalEvents.Periods.push(randomPeriodService(today))
-ecoCalEvents.Periods.push(randomPeriodService(tomorrow))
-ecoCalEvents.Periods.push(randomPeriodService(afterTomorrow))
+randomEcocalService()
 
 var addEvents = function () {
   $('.fxs_ecocal_event_row').click(function () {
@@ -158,4 +162,4 @@ var renderEcocal = function () {
     renderData()
   })
 }
-renderEcocal();
+renderEcocal()
